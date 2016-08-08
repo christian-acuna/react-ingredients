cdbb71242939037655a172248a79f1382f3cdb7a
Christian Acuna <cacuna0828@gmail.com>
2016-08-08 17:26:53 +0800

    Create react seed template

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var ListA = require('./components/List.jsx');
+
+
+ReactDOM.render(<List />, document.getElementById('ingredients'));
